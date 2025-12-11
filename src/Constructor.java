13fb3c221fd4ffa1726d5c96e6fa193d8bc1a55f
XAIMOH001 <xaimohchau@gmail.com>
2025-12-11 17:59:10 +0300

    practised the 'this' keyword

--- src/Constructor.java
+++ src/Constructor.java
@@ -13,7 +13,7 @@ public class Constructor {
 }
 */
 
-//constructor parameters
+/*//constructor parameters
 public class Constructor{
     String modelName;
     int makeYear;
@@ -26,6 +26,24 @@ public class Constructor{
     public static void main(String[] args){
         Constructor car1 = new Constructor(2025, "Rolls Royce");
         System.out.println("my " + car1.modelName + " " + car1.makeYear + " model");
+    }
+}
+ */
+
+//this keyword
+public class Constructor{
+    int x;
+    int y;
+
+    public Constructor(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void main(String[] args){
+        Constructor character = new Constructor(5, 8);
+        System.out.println(character.y);
+        System.out.println(character.x);
     }
 }
 
