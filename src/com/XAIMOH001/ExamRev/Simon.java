a0fcb82ae8f3b930f48a670b5b09af33891b2a1b
XAIMOH001 <xaimohchau@gmail.com>
2026-01-31 05:18:14 +0300

    practiced loops

---
+++ src/com/XAIMOH001/ExamRev/Simon.java
@@ -0,0 +1,15 @@
+package com.XAIMOH001.ExamRev;
+
+public class Simon {
+    public static void main(String[] args) {
+
+    }
+
+    public static void sayName() {
+        System.out.println("Simon");
+    }
+
+    public static void sayAge() {
+        System.out.println(23);
+    }
+}
